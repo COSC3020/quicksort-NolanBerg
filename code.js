293622a40911009311df68662287badb18487ed0
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-25 23:14:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function quicksort(array) {
-    return array;
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const pivot = array[Math.floor(array.length / 2)];
+    const left = [];
+    const right = [];
+
+    for (const element of array) {
+        if (element < pivot) {
+            left.push(element);
+        } else if (element > pivot) {
+            right.push(element);
+        }
+    }
+
+    return quicksort(left).concat(pivot, quicksort(right));
 }
+
+module.exports = { quicksort };
